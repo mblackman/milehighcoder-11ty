42aa701721b8077e464d88426dede24987ff2cb0
Matt Blackman <mateoblackman@gmail.com>
2024-12-01 16:30:45 -0700

    Improved projects page

--- .eleventy.js
+++ .eleventy.js
@@ -139,6 +139,19 @@ module.exports = function (eleventyConfig) {
         }
     });
 
+    eleventyConfig.addNunjucksFilter("contrastColor", function(bgColor) {
+        // Convert the background color to RGB
+        const r = parseInt(bgColor.slice(1, 3), 16);
+        const g = parseInt(bgColor.slice(3, 5), 16);
+        const b = parseInt(bgColor.slice(5, 7), 16);
+    
+        // Calculate the perceived brightness of the color
+        const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+    
+        // Return either black or white depending on the brightness
+        return brightness > 128 ? "black" : "white";
+      });
+
     eleventyConfig.addShortcode("image", async function (src, alt, sizes) {
         let metadata = await Image(src, {
             widths: [300, 600],
